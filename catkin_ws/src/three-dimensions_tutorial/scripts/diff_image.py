91402618288994fb309f6113f7e2a56d8a4bddbc
Adwaver4157 <adwaver4157>
2022-09-21 14:26:41 +0900

    raw and diff image

--- catkin_ws/src/three-dimensions_tutorial/scripts/diff_image.py
+++ catkin_ws/src/three-dimensions_tutorial/scripts/diff_image.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-from tracemalloc import start
+from math import pi
 import rospy
+import tf
 import message_filters
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 from pytorchyolo import detect, models
 import matplotlib.pyplot as plt
@@ -20,10 +23,12 @@ class DetectionDistance:
 
         # Publisher
         self.detection_result_pub = rospy.Publisher('/detection_result', Image, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # Subscriber
         rgb_sub = message_filters.Subscriber('/camera/color/image_raw', Image)
         depth_sub = message_filters.Subscriber('/camera/aligned_depth_to_color/image_raw', Image)
+        odom_sub = rospy.Subscriber('/odom', Odometry, self.callback_odom)
         message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], 10, 1.0).registerCallback(self.callback_rgbd)
 
         self.bridge = CvBridge()
@@ -37,6 +42,43 @@ class DetectionDistance:
         cv_array = self.bridge.imgmsg_to_cv2(data2, 'passthrough')
         self.depth_image = cv_array
 
+    def callback_odom(self, data):
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        self.yaw = self.get_yaw_from_quaternion(data.pose.pose.orientation)
+
+    def go_straight(self, dis, velocity=0.3):
+        vel = Twist()
+        x0 = self.x
+        y0 = self.y
+        while (np.sqrt((self.x-x0)**2+(self.y-y0)**2) < dis):
+            vel.linear.x = velocity
+            vel.angular.z = 0.0
+            self.cmd_vel_pub.publish(vel)
+            rospy.sleep(0.1)
+        self.stop()
+
+    def turn(self, yaw, yawrate):
+        vel = Twist()
+        yaw0 = self.yaw
+        while (abs(self.yaw-yaw0) < np.deg2rad(yaw)):
+            vel.linear.x = 0.0
+            vel.angular.z = yawrate
+            self.cmd_vel_pub.publish(vel)
+            rospy.sleep(0.1)
+        self.stop()
+
+    def stop(self):
+        vel = Twist()
+        vel.linear.x = 0.0
+        vel.angular.z = 0.0
+        self.cmd_vel_pub.publish(vel)
+
+    def get_yaw_from_quaternion(self, quaternion):
+        e = tf.transformations.euler_from_quaternion(
+            (quaternion.x, quaternion.y, quaternion.z, quaternion.w))
+        return e[2]
+
     def process(self):
         image_path = "/root/roomba_hack/catkin_ws/src/three-dimensions_tutorial/images/"
         path = "/root/roomba_hack/catkin_ws/src/three-dimensions_tutorial/yolov3/"
@@ -49,11 +91,11 @@ class DetectionDistance:
         model = models.load_model(path+"config/yolov3.cfg", path+"weights/yolov3.weights")
 
         x1, y1, x2, y2 = (0, 0, 0, 0)
-        start_time = time.time()
+        start_time = float('inf')
         pre_image = None
         is_found_person = False
         sum_diff = 0
-        while not rospy.is_shutdown():
+        while not rospy.is_shutdown() and time.time()-start_time < 10:
             if self.rgb_image is None:
                 continue
             # inference
@@ -62,27 +104,43 @@ class DetectionDistance:
             # [[x1, y1, x2, y2, confidence, class]]
             for box in boxes:
                 cls_pred = category[int(box[5])]
-                if cls_pred=='person':
+                if cls_pred == 'person':
                     if is_found_person:
                         trimed_image = rgb_image[y1:y2, x1:x2, :]
                         diff_image = cv2.absdiff(trimed_image, pre_image)
                         diff_gray_image = cv2.cvtColor(diff_image, cv2.COLOR_RGB2GRAY)
                         ret, diff_bin_image = cv2.threshold(diff_gray_image, 20, 255, cv2.THRESH_BINARY)
+                        cv2.imwrite(image_path+'raw.png', trimed_image)
                         cv2.imwrite(image_path+"diff.png", diff_bin_image)
                         sum_diff += diff_bin_image.sum()
-                        print(sum_diff)
                         pre_image = trimed_image
                     else:
                         is_found_person = True
                         print('found person')
                         x1, y1, x2, y2 = map(int, box[:4])
-                        pre_image=rgb_image[y1:y2, x1:x2, :]
+                        start_time = time.time()
+                        pre_image = rgb_image[y1:y2, x1:x2, :]
 
             # publish image
             result_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
             detection_result = self.bridge.cv2_to_imgmsg(result_image, "bgr8")
             self.detection_result_pub.publish(detection_result)
             self.rgb_image = None
+
+        print(sum_diff)
+        if sum_diff > 1500000:
+            cx = (x1+x2)/2
+            if cx > rgb_image.shape[1]/2:
+                self.turn(10, -0.5)
+            else:
+                self.turn(10, 0.5)
+        else:
+            cx = (x1+x2)/2
+            if cx > rgb_image.shape[1]/2:
+                self.turn(10, 0.5)
+            else:
+                self.turn(10, -0.5)
+        self.go_straight(2.0)
 
 
 if __name__ == '__main__':
